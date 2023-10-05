a16937720b1375b82634de186eb6a3589c1cd895
AsGuizar <angelsguizar2004@gmail.com>
2023-10-05 17:47:04 -0600

    Working wirh Recursivity
    Give it a check, this is quite a showcase of my logic thinking when it comes to coding.

---
+++ Programación 3/Estructura de datos/ConteoCartas.js
@@ -0,0 +1,34 @@
+    var conteo = 0;
+
+    function contarCartas (carta){
+        var decision;
+
+        switch (carta) {
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                conteo++;
+                break;
+            
+            case 10:
+            case "J":
+            case "Q":
+            case "K":
+            case "A":
+                conteo--;
+                break;
+        }
+        if (conteo > 0 ) { 
+          decision = "apostar";
+        } else { 
+                decision = "esperar";
+        }
+        return conteo + " " + decision
+    }
+
+    console.log(contarCartas(2))
+    console.log(contarCartas(3))
+    console.log(contarCartas("K"))
+    console.log(contarCartas("Q"))
