68ebc158243e2524df60747480f2c3c70ac0f8d4
AsGuizar <angelsguizar2004@gmail.com>
2024-05-27 21:40:22 -0600

    updates
    irrelevant

--- Store/authentication/backend/config/Database.js
+++ Store/authentication/backend/config/Database.js
@@ -3,7 +3,16 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize('auth_db', 'root', 'root', {
     host: "localhost",
     dialect: "mysql",
-    port: 3310 // Specify the MySQL port explicitly
+    port: 3310 
 });
+
+// Test the connection
+db.authenticate()
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 export default db;
