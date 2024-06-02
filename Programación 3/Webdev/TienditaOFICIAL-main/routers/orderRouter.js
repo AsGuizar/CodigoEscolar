fdde0cd1758d845133d7aef470fa5f485ca3a37e
AsGuizar <angelsguizar2004@gmail.com>
2024-06-02 17:54:51 -0600

    Just cleaning dust here.

--- Programación 3/Webdev/TienditaOFICIAL-main/routers/orderRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const orderController = require('../controllers/orderController');
-const authenticateJWT = require('../middleware/authenticateJWT');
-
-const router = express.Router();
-
-router.post('/', authenticateJWT.isLoggedIn, orderController.createOrder);
-
-module.exports = router;
